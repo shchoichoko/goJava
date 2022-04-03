0413592b6fff4705e4ba811efa1306568e4210f7
shchoi <shchoicoder@gmail.com>
2022-04-03 10:39:19 +0900

    modified array variable

--- prac/WakeUpStupid/src/Main.java
+++ prac/WakeUpStupid/src/Main.java
@@ -10,14 +10,16 @@ public class Main {
 	
 	public int getWakeUpTime() {	// 기상 분수를 구하는 함수
 		
-		int minutes[] = new int[5];
+		int minutes[] = new int[177/11];
 		int wakeUpMinutes = 0;
 		for (int i = 5; i < 151; i = i + 10) {	// 철수는 10분 간격으로 M5분에 맞춰서 일어남.
 
 			for (int j = 10; j < 177; j = j + 11) {	
 				// 버스 정류장 버스 도착 시작 시간이 10분부터 11분 간격
-				if (0 == Math.abs(j - i - 3)) {	
-					minutes[0] = i;				
+				for(int time = 0; time<(177-10)/11; time++) {
+					if (0 == Math.abs(j - i - 3)) {	
+						minutes[time] = i;				
+					}
 				}
 			}
 		}
