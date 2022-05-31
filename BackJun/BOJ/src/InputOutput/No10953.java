c06e0f000f1640b4d4d277c57279efa5ecda290b
shchoi <shchoicoder@gmail.com>
2022-05-31 23:24:17 +0900

    입출력 알고리즘 풀이.

--- BackJun/BOJ/src/InputOutput/No10953.java
+++ BackJun/BOJ/src/InputOutput/No10953.java
@@ -8,14 +8,14 @@ public class No10953 {
 		//첫째 줄에 테스트 케이스의 개수 T가 주어진다.
 		//각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. A와 B는 콤마(,)로 구분되어 있다. (0 < A, B < 10)
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		System.out.println("");
-		String A = " ";
-		int C = 0;
+		int T = Integer.parseInt(sc.nextLine());
+		String a = "";
+		int c = 0,d = 0;
 		for( int i = 0; i < T; i++) {
-			A = sc.nextLine();
-			
-			System.out.println(A);
+			a = sc.nextLine();
+			c = Integer.parseInt(a.substring(0,1));
+			d = Integer.parseInt(a.substring(2,3));
+			System.out.println(c+d);
 		}
 		
 		
