87e866a87818e2f92a3946f9b304438a226fab54
shchoi <101681471+shchoichoko@users.noreply.github.com>
2022-08-02 15:45:04 +0900

    연필의 수

--- BackJun/PaizaJava/src/LevelD/NumberOfPencil.java
+++ BackJun/PaizaJava/src/LevelD/NumberOfPencil.java
@@ -2,14 +2,14 @@ package LevelD;
 
 import java.util.Scanner;
 
-public class MinuteAndSecond {
+public class NumberOfPencil {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
         int lineToInt = Integer.parseInt(line);
-        int result = lineToInt * 60;
+        int result = lineToInt * 12;
         System.out.println(result);
 	}
 
