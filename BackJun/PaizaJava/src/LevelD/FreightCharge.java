7bcb1ae41ecedd0e9e9a1706ab7a81c2da6bf068
shchoi <shchoicoder@gmail.com>
2022-08-04 23:54:46 +0900

    FreightCharge

---
+++ BackJun/PaizaJava/src/LevelD/FreightCharge.java
@@ -0,0 +1,13 @@
+package LevelD;
+
+import java.util.Scanner;
+
+public class FreightCharge {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        System.out.println(100+(first*10));
+	}
+
+}
