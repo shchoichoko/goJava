0a4ef9160aac4761ed383c62ec267885b87e92dd
shchoi <shchoicoder@gmail.com>
2022-08-04 23:48:16 +0900

    NameOfCharacter

---
+++ BackJun/PaizaJava/src/LevelD/NameOfCharacter.java
@@ -0,0 +1,15 @@
+package LevelD;
+
+import java.util.Scanner;
+
+public class NameOfCharacter {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int result = name.length();
+        System.out.println(result);
+
+	}
+
+}
