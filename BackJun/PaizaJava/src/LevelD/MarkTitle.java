2b70b9937da4b31eb846f637cb35962d0eacd010
shchoi <101681471+shchoichoko@users.noreply.github.com>
2022-08-05 15:39:57 +0900

    Paiza 문제풀이

---
+++ BackJun/PaizaJava/src/LevelD/MarkTitle.java
@@ -0,0 +1,14 @@
+package LevelD;
+
+import java.util.Scanner;
+
+public class MarkTitle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		String line2 = sc.nextLine();
+		System.out.println(line+"/"+line2);
+	}
+
+}
---
+++ BackJun/PaizaJava/src/LevelD/TwoButtons.java
@@ -0,0 +1,15 @@
+package LevelD;
+
+import java.util.Scanner;
+
+public class TwoButtons {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int first = sc.nextInt();
+		int second = sc.nextInt();
+        System.out.println(first-second);
+
+	}
+
+}
