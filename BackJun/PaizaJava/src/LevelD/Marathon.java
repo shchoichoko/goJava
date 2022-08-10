71b74243f7ab15c483c766a0fb8c188bbc10a9a6
shchoi <101681471+shchoichoko@users.noreply.github.com>
2022-08-10 09:55:55 +0900

    paiza 문제 풀이

---
+++ BackJun/PaizaJava/src/LevelD/Marathon.java
@@ -0,0 +1,14 @@
+package LevelD;
+
+import java.util.Scanner;
+
+public class Marathon {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int line = sc.nextInt();
+		int line2 = sc.nextInt();
+		System.out.println(line*line2);
+	}
+
+}
