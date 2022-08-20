38e357b0b0a65c365b6fbe21ac9ab8c965825285
shchoi <shchoicoder@gmail.com>
2022-08-20 09:23:33 +0900

    GetPlayer

--- BackJun/Programmers/src/GetPlayer.java
+++ BackJun/Programmers/src/GetPlayer.java
@@ -8,21 +8,31 @@ public class GetPlayer {
 	}
 
     public static int[] solution(int[] answers) {
-        int[] answer = {};
-        int answerLength = answer.length;
+
+        int answerLength = answers.length;
         int[] firstWay = {1, 2, 3, 4, 5};
         int[] secondWay = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] thirdWay = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] firstMan = new int[answerLength];
-        int[] secondMan = new int[answerLength];
-        int[] thirdMan = new int[answerLength];
-        
-        for(int i = 0; i < answerLength; i++) {
-        	
+        int[][] ways = {{1, 2, 3, 4, 5},{2, 1, 2, 3, 2, 4, 2, 5},{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[] firstAnswer = new int[answerLength];
+        int[] secondAnswer = new int[answerLength];
+        int[] thirdAnswer = new int[answerLength];
+        int[] cycle = {answerLength/firstWay.length,answerLength/secondWay.length,answerLength/thirdWay.length};
+        int[] rest =  {answerLength%firstWay.length,answerLength%secondWay.length,answerLength%thirdWay.length};
+        for(int i = 0; i < 3; i++) {
+        	System.out.println("몫 : "+cycle[i]);
+        	System.out.println("나머지 : "+rest[i]);
+        }
+        //수포자 3명 전부 답 넣기 위함
+        for(int i = 0; i < 3; i++) {
+        	for(int j = 0; j < ways[i].length; j++ ) {
+        		firstAnswer[j] = ways[i][j];
+        	}
+
         }
         
         
         
-        return answer; 
+        return answers; 
     }
 }
